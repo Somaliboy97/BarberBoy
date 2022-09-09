8b0256082c9c35a89aacec2589a9ec35798d318e
Greemi <geniuszek22@gmail.com>
2022-09-09 22:26:10 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+const nav = document.querySelector('nav ul');
+const menuIcon = document.querySelector('.open-menu');
+const closeIcon = document.querySelector('.close-menu');
+
+menuIcon.addEventListener('click', () => {
+    nav.classList.add('active');
+    closeIcon.classList.add('active');
+    menuIcon.classList.add('active')
+});
+
+closeIcon.addEventListener('click', () => {
+    nav.classList.remove('active');
+    closeIcon.classList.remove('active');
+    menuIcon.classList.remove('active')
+});
+
+// Scroll Nav
+
+const navScroll = document.querySelector('nav');
+
+window.addEventListener('scroll', () => {
+    if(window.pageYOffset > 60) {
+        navScroll.classList.add('active');
+    } else {
+        navScroll.classList.remove('active');
+    };
+});
